3cd1423d950f14acd3e195552feb602d8853e5f9
Jefferson Denilson <jeffersondenilson8@gmail.com>
2022-04-16 11:12:05 -0300

    styles for navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./Navbar.css";
+
 function Navbar() {
   return (
     <nav>
-      <div>
-        <Link to="/">E-commerce</Link>
-      </div>
-      <div>
+      <div style={{ marginLeft: "16px" }}>
         <Link to="/">Produtos</Link>
       </div>
       <div>
